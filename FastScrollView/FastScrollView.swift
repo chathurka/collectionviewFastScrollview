be900f4a50ef250abead6fce6ae03c0ed987522d
Roshan <roshan@jrcsoftware.com>
2019-01-17 00:36:03 +0530

    Added files.

--- FastScrollView/FastScrollView.swift
+++ FastScrollView/FastScrollView.swift
@@ -8,6 +8,72 @@
 
 import UIKit
 
-class FastScrollView: NSObject {
-
+class FastScrollView: UIView {
+    @IBOutlet weak var table: UITableView? {
+        didSet {
+            self.table?.tableFooterView = UIView()
+            self.table?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        }
+    }
+    
+    var alphabatics:[String] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var view: UIView!
+    let cellName = "FastScrollView"
+    
+    var didSelectIndexTitle:((String)->())?
+    
+    convenience init(frame: CGRect, names:[String]) {
+        self.init(frame: frame)
+        self.alphabatics.removeAll()
+        self.alphabatics.append(contentsOf: names)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of:self))
+        let nib = UINib(nibName: self.cellName, bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        return view
+    }
 }
+
+extension FastScrollView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.alphabatics.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.alphabatics[indexPath.row]
+        
+        return cell
+    }
+}
+
+// MARK:- UITableViewDelegate
+
+extension FastScrollView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let closure = self.didSelectIndexTitle {
+            closure(self.alphabatics[indexPath.row])
+        }
+    }
+}
+
+
--- FastScrollView/ViewController.swift
+++ FastScrollView/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let innerView = FastScrollView(frame: CGRect(x: 0, y: 0, width: 20, height: 100),names:["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+        self.view.addFastScrollView(view: innerView)
     }
-
-
 }
 
+
+extension UIView {
+    func addFastScrollView (view:UIView) {
+        view.frame = CGRect(x: Int(self.frame.size.width - 20), y: 0, width: 20, height: Int(self.frame.size.height))
+        self.addSubview(view)
+    }
+}
