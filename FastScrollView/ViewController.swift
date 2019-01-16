b95cf8c6002027d11f92e612e2008f540d4a7626
Roshan <roshan@jrcsoftware.com>
2019-01-16 23:27:15 +0530

    Initial Commit

---
+++ FastScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastScrollView
+//
+//  Created by Chathurka Roshan on 1/16/19.
+//  Copyright © 2019 Chathurka Roshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
