93a050a2727b6f8687bc0fa19d8d6e0c5f310ef8
Roshan <roshan@jrcsoftware.com>
2019-01-17 00:33:57 +0530

    first commit

---
+++ FastScrollView/FastScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  FastScrollView.swift
+//  FastScrollView
+//
+//  Created by Chathurka Roshan on 1/16/19.
+//  Copyright © 2019 Chathurka Roshan. All rights reserved.
+//
+
+import UIKit
+
+class FastScrollView: NSObject {
+
+}
